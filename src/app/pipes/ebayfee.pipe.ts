09445b3581c4de674d73c87c61c68a93319d5ea6
mgroshevoy <mgroshevoy@gmail.com>
2017-05-30 21:12:39 +0200

    Fixes

--- src/app/pipes/ebayfee.pipe.ts
+++ src/app/pipes/ebayfee.pipe.ts
@@ -8,7 +8,7 @@ export class EbayfeePipe implements PipeTransform {
   transform(value: any, args?: any): any {
     let ebayFee = 0;
     if (value.order_status === 'Cancelled' || (value.order_status === 'Active' && value.status === 'Incomplete')) {
-      ebayFee = 0.3;
+      ebayFee = 0;
     } else {
       for (const val of value.items) {
         ebayFee += Number(val.FinalValueFee);
--- src/app/pipes/paypalfee.pipe.ts
+++ src/app/pipes/paypalfee.pipe.ts
@@ -8,7 +8,7 @@ export class PaypalfeePipe implements PipeTransform {
   transform(value: any, args?: any): any {
     let paypalFee: number;
     if (value.order_status === 'Cancelled' || (value.order_status === 'Active' && value.status === 'Incomplete')) {
-      paypalFee = 0;
+      paypalFee = 0.3;
     } else {
       paypalFee = value.total * 0.044;
     }
